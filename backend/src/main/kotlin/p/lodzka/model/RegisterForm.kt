581d1db2139132969326f525bf32eb37a268dba9
ancalentari <jaroslawpawlak742@gmail.com>
2019-04-11 23:31:45 +0200

    fix register form

--- backend/src/main/kotlin/p/lodzka/model/RegisterForm.kt
+++ backend/src/main/kotlin/p/lodzka/model/RegisterForm.kt
@@ -1,8 +1,8 @@
 package p.lodzka.model
 
-class RegisterForm {
-    var name: String? = null
-    var email: String? = null
-    var password: String? = null
+class RegisterForm (
+    var name: String? = null,
+    var email: String? = null,
+    var password: String? = null,
     var repeatPassword: String? = null
-}
+)
