9de25155321f79aef7b5c27901f816315fe81266
90rl <radoslaw.lt14@gmail.com>
2019-05-05 01:25:23 +0200

    wprowadzone hasło jest inne niż to w bazie

--- backend/src/main/kotlin/p/lodzka/processor/UserValidationProcessor.kt
+++ backend/src/main/kotlin/p/lodzka/processor/UserValidationProcessor.kt
@@ -1,4 +1,26 @@
 package p.lodzka.processor
 
-class UserValidationProcessor {
+import org.apache.camel.Exchange
+import org.apache.camel.Processor
+import p.lodzka.config.APPLICATION_JSON
+import p.lodzka.form.RegisterForm
+import p.lodzka.service.objects.ErrorResponse
+import javax.servlet.http.HttpServletResponse
+
+
+class UserValidationProcessor : Processor {
+    override fun process(exchange: Exchange) {
+        val form = exchange.getIn().getBody(RegisterForm::class.java)
+        if (form.password.isBlank()  || form.repeatPassword.isBlank()){
+            exchange.getIn().setHeader(Exchange.HTTP_RESPONSE_CODE, HttpServletResponse.SC_BAD_REQUEST)
+            exchange.getIn().setHeader(Exchange.CONTENT_TYPE, APPLICATION_JSON)
+            exchange.getIn().body = ErrorResponse(errorId = "UVP_TR001" , description = "Wpisz hasło")
+        }
+        if(form.password != form.repeatPassword){
+            exchange.getIn().setHeader(Exchange.HTTP_RESPONSE_CODE, HttpServletResponse.SC_BAD_REQUEST)
+            exchange.getIn().setHeader(Exchange.CONTENT_TYPE, APPLICATION_JSON)
+            exchange.getIn().body = ErrorResponse(errorId = "UVP_TR002" , description = "Powtórzone hasło nie jest takie samo")
+        }
+    }
+
 }
